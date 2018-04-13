a4a9936d7c614681f57283258d31a07c75c2cbc9
DESKTOP-5F1ACM6\monica <leandro18195@hotmail.com>
2018-04-13 19:59:43 -0300

    modificaciones en DataBase

--- JDictionaryFoxtrot/src/main/java/ayds/dictionary/foxtrot/model/DataBase.java
+++ JDictionaryFoxtrot/src/main/java/ayds/dictionary/foxtrot/model/DataBase.java
@@ -3,94 +3,53 @@ package ayds.dictionary.foxtrot.model;
 import java.sql.*;
 
 public class DataBase {
+  private static final String pathConnection = "jdbc:sqlite:./dictionary.db";
+
+  private static Connection conectarBD() {
+     Connection connection= null;
+    try {
+       connection = DriverManager.getConnection(pathConnection);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return connection;
+  }
 
   public static void createNewDatabase() {
-
-    String url = "jdbc:sqlite:./dictionary.db";
-
-    try (Connection connection = DriverManager.getConnection(url)) {
-      if (connection != null) {
-        DatabaseMetaData meta = connection.getMetaData();
-
+    try {
+      Connection connection = conectarBD();
+      if (connection!=null) {
         Statement statement = connection.createStatement();
         statement.setQueryTimeout(30);  // set timeout to 30 sec.
-
         statement.executeUpdate("create table terms (id INTEGER PRIMARY KEY AUTOINCREMENT, term string, meaning string, source integer)");
 
-      }
-
-    } catch (SQLException e) {
-    }
+       }
+     }catch (SQLException e) { }
   }
 
-  public static void saveTerm(String term, String meaning)
-  {
-    Connection connection = null;
-    try
-    {
-      // create a database connection
-      connection = DriverManager.getConnection("jdbc:sqlite:./dictionary.db");
-      Statement statement = connection.createStatement();
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
-
-      System.out.println("INSERT  " + term + "', '"+ meaning );
-
-      statement.executeUpdate("insert into terms values(null, '"+ term + "', '"+ meaning + "', 1)");
-    }
-    catch(SQLException e)
-    {
-      System.err.println("Error saving " + e.getMessage());
-    }
-    finally
-    {
-      try
-      {
-        if(connection != null)
-          connection.close();
+  public static void saveTerm(String term, String meaning) {
+    try {
+      Connection connection = conectarBD();
+      if (connection!=null) {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("insert into terms values(null, '" + term + "', '" + meaning + "', 1)");
+        connection.close();
       }
-      catch(SQLException e)
-      {
-        // connection close failed.
-        System.err.println( e);
-      }
-    }
+    }catch(SQLException e) { }
   }
 
-  public static String getMeaning(String term)
-  {
-
-    Connection connection = null;
-    try
-    {
-      // create a database connection
-      connection = DriverManager.getConnection("jdbc:sqlite:./dictionary.db");
-      Statement statement = connection.createStatement();
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
-
-      ResultSet rs = statement.executeQuery("select * from terms WHERE term = '" + term + "'" );
-      rs.next();
-      return rs.getString("meaning");
-    }
-    catch(SQLException e)
-    {
-      // if the error message is "out of memory",
-      // it probably means no database file is found
-      System.err.println("Get term error " + e.getMessage());
-    }
-    finally
-    {
-      try
-      {
-        if(connection != null)
-          connection.close();
+  public static String getMeaning(String term) {
+    String meaning = null;
+    try {
+      Connection connection = conectarBD();
+      if (connection!=null) {
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("select * from terms WHERE term = '" + term + "'");
+        rs.next();
+        meaning= rs.getString("meaning");
+        connection.close();
       }
-      catch(SQLException e)
-      {
-        // connection close failed.
-        System.err.println(e);
-      }
-    }
-    return null;
+    }catch(SQLException e) { }
+    return meaning;
   }
-
 }
