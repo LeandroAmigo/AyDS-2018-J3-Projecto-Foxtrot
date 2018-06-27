e435e2991d5ac4189918292cc81a5e694009dfa2
DESKTOP-5F1ACM6\monica <leandro18195@hotmail.com>
2018-06-27 09:52:21 -0300

    cambios requeridos

--- JDictionaryFoxtrot/src/main/java/ayds/dictionary/foxtrot/model/TranslatorModelModule.java
+++ JDictionaryFoxtrot/src/main/java/ayds/dictionary/foxtrot/model/TranslatorModelModule.java
@@ -2,6 +2,9 @@ package ayds.dictionary.foxtrot.model;
 
 import ayds.dictionary.foxtrot.model.databases.*;
 import ayds.dictionary.foxtrot.model.externalServices.ServiceDefModule;
+import ayds.dictionary.foxtrot.model.externalServices.Source;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TranslatorModelModule {
   private static TranslatorModelModule instance;
@@ -10,9 +13,10 @@ public class TranslatorModelModule {
 
   private TranslatorModelModule() {
     DataBase dataBase= TranslatorDatabasesModule.getInstance().getDataBase();
+    Map<Source, Exception> exceptionMap = new HashMap<Source, Exception>();
     exceptionHandler = new ExceptionHandlerImpl();
     translatorModel =  new TranslatorModelImpl(
-        new RepositoryImpl(dataBase, ServiceDefModule.getInstance().getServiceDef()));
+        new RepositoryImpl(dataBase, ServiceDefModule.getInstance().getServiceDef(), exceptionMap, exceptionHandler));
   }
 
   public static TranslatorModelModule getInstance() {
